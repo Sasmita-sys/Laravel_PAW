874b300ed67c6c0905838019a009c1e1f72e2e75
Sasmita-sys <79735970+Sasmita-sys@users.noreply.github.com>
2021-05-04 21:13:37 +0700

    Add files via upload

---
+++ mahasiswas.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 04, 2021 at 03:40 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tugas-laravel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswas`
+--
+
+CREATE TABLE `mahasiswas` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nim` varchar(18) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `mahasiswas`
+--
+
+INSERT INTO `mahasiswas` (`id`, `nim`, `nama`, `email`, `created_at`, `updated_at`) VALUES
+(1, '185150300111005', 'Sasmita Eko Raharjo', 'sasmita@gmail.com', '2021-05-04 13:37:00', NULL),
+(2, '185150400111004', 'Mirana', 'mira31@yahoo.com', '2021-05-04 13:37:00', NULL),
+(3, '185150300111001', 'Dendi', 'dendeee@gmail.com', '2021-05-04 13:38:42', '2021-05-04 06:39:35'),
+(4, '185150400111003', 'Ralph B', 'boneRalph@gmail.com', '2021-05-04 13:38:42', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswas`
+--
+ALTER TABLE `mahasiswas`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `mahasiswas_nim_unique` (`nim`),
+  ADD UNIQUE KEY `mahasiswas_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mahasiswas`
+--
+ALTER TABLE `mahasiswas`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
